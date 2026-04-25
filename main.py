13f3138d20a050ceadfe697a98fd3d0af0911543
Михаил Шахов <mishanya@Mihails-MacBook-Air-2.local>
2026-04-25 18:28:45 +0300

    Refactor: extracted sub check to function and fixed await bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
 load_dotenv()
@@ -11,20 +11,28 @@ TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     exit("Ошибка: BOT_TOKEN не найден в файле .env")
 
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 if not CHANNEL_ID:
     exit("Ошибка: CHANNEL_ID не найден в файле .env")
+else:
+    CHANNEL_ID = int(CHANNEL_ID)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+async def check_sub(user_id):
+    chat_member = await bot.get_chat_member(chat_id = CHANNEL_ID, user_id = user_id)
+    if chat_member.status not in ["left", "kicked"]:
+        return True
+    else:
+        return False
+
 @dp.callback_query(F.data == "check_subs")
 async def check_callback(callback: CallbackQuery):
-    chat_member = await bot.get_chat_member(chat_id = CHANNEL_ID, user_id = callback.from_user.id)
-    if chat_member.status not in ["left", "kicked"]:
+    if await check_sub(callback.from_user.id):
         await callback.message.answer("Твой промокод:")
     else:
-        await callback.answer("Ты все еще не подписан!", show_allert=True)
+        await callback.answer("Ты все еще не подписан!", show_alert=True)
     await callback.answer()
 
 @dp.message(CommandStart())
@@ -33,8 +41,7 @@ async def hello(message: Message):
 
 @dp.message(Command("get_promo"))
 async def promo(message: Message):
-    chat_member = await bot.get_chat_member(chat_id = CHANNEL_ID, user_id = message.from_user.id)
-    if chat_member.status != "left" and chat_member.status != "kicked":
+    if await check_sub(message.from_user.id):
         await message.answer("Твой промокод:")
     else:
         link_button = InlineKeyboardButton(text="Подписаться на канал", url="https://t.me/testtesttest232312")
